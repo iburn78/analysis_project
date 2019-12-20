8d5f16b356c2eaa5e8cb38153415c8b452abea9c
Andy Kim <andy.chiwon.kim@gmail.com>
2019-12-20 17:26:24 +0900

    wip

--- test/json_test.py
+++ test/json_test.py
@@ -6,10 +6,30 @@ import pandas as pd
 # WRITE TO PANDAS AND SAVE IT TO JSON (I may need Json editor for this purpose)
 # JSON FILE NAME: fields.json
 
-df = pd.DataFrame([['a','b'], ['c','d']], 
-index = ['row 1', 'row 2'], columns = ['col 1', 'col 2'])
+df = pd.DataFrame(
+    [{
+        '종목코드': ['stockcode', 'varchar(100)'],
+        '종목명': ['stockname', 'varchar(100)'],
+        '매수거래량': ['qbuy', 'float'],
+        '매도거래량': ['qsell', 'float'],
+        '순매수거래량': ['qbuy_net', 'float'],
+        '매수거래대금': ['pbuy', 'float'],
+        '매도거래대금': ['psell', 'float'],
+        '순매수거래대금': ['pbuy_net', 'float'],
+        '업종명': ['business_area', 'varchar(100)'],
+    }], 
+    columns = ['from', 'to', 'type'])
+df.to_json('fields.json', orient='split')
 
 # %%
 
-df.to_json(orient='split')
-pd.read_json(_, orient='split')
+fields = pd.read_json('fields.json', orient='split')
+print(fields)
+
+test_db = pd.read_excel('data.xls', thousands=',')
+print(test_db)
+
+#%%
+for i in test_db.columns:
+    print(fields)
+    print(i)
