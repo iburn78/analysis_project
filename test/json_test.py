2871669af064ae0456f202e7043f0e88af268af4
iburn78 <andy.chiwon.kim@gmail.com>
2019-12-24 10:43:36 +0900

    wip

--- test/json_test.py
+++ test/json_test.py
@@ -31,10 +31,40 @@ test_db = pd.read_excel('data.xls', thousands=',')
 
 #%%
 for i in test_db.columns:
-    
-    print(fields)
     print(i)
 
 fields.loc[fields['from'] == '종목명'].to.values
+
+# %%
+df = test_db 
+df = df.rename(columns = fields.set_index('from')['to'].to_dict())
+
+col_text = '('
+for i in range(len(df.columns)):
+    col_text += f'{df.columns[i]}, '
+col_text = col_text[:-2] + ')'
+
+val_text = '('
+for i in range(len(df.columns)):
+    if fields.set_index('to')[df.columns[i]]
+    val_text += 
+
+
+# for i in range(len(df)):
+a1 = str(df['stockcode'][i])
+a2 = str(df['stockname'][i])
+a3 = float(df['qbuy'][i])
+a4 = float(df['qsell'][i])
+a5 = float(df['qbuy_net'][i]) 
+a6 = float(df['pbuy'][i])
+a7 = float(df['psell'][i])
+a8 = float(df['pbuy_net'][i])
+# a9 = str(df['business_area'][i]) # skipping saving a9 intentionally
+a10 = str(df['date'][i])
+insert_query=f'''
+INSERT INTO {tb}
+(stockcode, stockname, qbuy, qsell, qbuy_net, pbuy, psell, pbuy_net, date)
+VALUES		('{a1}', '{a2}', {a3}, {a4}, {a5}, {a6}, {a7}, {a8}, '{a10}');
+            '''
 
 # %%
